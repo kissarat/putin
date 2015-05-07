9dd4335d3abd4dfeb4a73c9c4ec2e44ec7c0b58d
Taras Labiak <kissarat@gmail.com>
2015-05-08 02:43:31 +0300

    + busy_wait, move http error handlers, Queue to deque

--- crawl.py
+++ crawl.py
@@ -1,37 +1,42 @@
-from base64 import b64encode
 from binascii import crc32
 from collections import deque
 from datetime import datetime
 from os import makedirs
 from os.path import isfile, isdir
 from queue import Queue
-from random import random
+from random import random, choice
 from re import compile as regex, IGNORECASE
 from signal import signal, SIGINT
 from sys import argv
 from threading import Thread
 from time import sleep
-from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.request import urlopen, Request
+from piston_mini_client import SocketError
+
+with open('browsers', encoding='utf-8') as f:
+    browsers = f.read().split('\n')
 
 
 class Crawler(Thread):
     work = False
     threads = []
+    threads_number = 36
     host = argv[1]
     root = 'http://%s/' % host
     archive = 'archive/' + host + '/'
-    last = datetime.now()
     wait_count = 0
+    busy_wait = 0
 
     @staticmethod
     def crawl():
         Crawler.work = True
-        for i in range(12):
+        for i in range(Crawler.threads_number):
             Crawler.threads.append(Crawler())
         Crawler.schedule('')
         for crawler in Crawler.threads:
-            sleep(random() * 2)
             crawler.start()
+            sleep(random() * 4)
 
     def __init__(self, *params):
         super().__init__(*params)
@@ -39,6 +44,8 @@ class Crawler(Thread):
         self.list = []
         self.file = None
         self.queue_filename = None
+        self.snooze_summary = 0
+        self.last = datetime.now()
 
     def run(self):
         number = self.threads.index(self)
@@ -46,7 +53,7 @@ class Crawler(Thread):
         filename = self.archive + str(number)
         self.queue_filename = filename + '.queue'
         self.list = self.load_list(filename)
-        self.queue = deque(self.load_list(filename) + list(self.queue))
+        self.queue = deque(self.load_list(self.queue_filename) + list(self.queue))
         self.file = open(filename, 'a')
         print('▨ ▨ start ' + self.name)
         while self.work:
@@ -61,18 +68,44 @@ class Crawler(Thread):
         print('▨ stop ' + self.name)
 
     def request(self):
+        snooze = 0
+        if self.busy_wait > 0 and len(self.queue) != 0:
+            snooze += 0.1 + random() * self.busy_wait/self.threads_number
+            self.busy_wait -= snooze
+            sleep(snooze)
+        if self.busy_wait < 1:
+            self.busy_wait = 0
         while 0 == len(self.queue):
             Crawler.wait_count += 1
-            sleep(random() * 2)
-            if 9 < Crawler.wait_count < 12:
+            snooze += random() * 2
+            sleep(snooze)
+            if Crawler.threads_number - 3 < Crawler.wait_count < Crawler.threads_number:
                 print('# queue almost empty ' + str(Crawler.wait_count))
-            if 12 == Crawler.wait_count:
+            if Crawler.threads_number == Crawler.wait_count:
                 print('EMPTY QUEUE')
                 return Crawler.shutdown()
             Crawler.wait_count -= 1
+        self.snooze_summary += snooze
         url = self.queue.pop()
-        r = urlopen(self.root + url)
-        r = r.read().decode('utf-8', 'ignore')
+
+        try:
+            r = urlopen(Request(self.root + url, headers={'User-Agent': choice(browsers)}))
+        except SocketError as ex:
+            self.busy_wait += self.threads_number
+            log(str(ex))
+            return
+        except HTTPError as ex:
+            if 503 == ex.code:
+                self.busy_wait += self.threads_number
+                log(str(ex))
+            elif 404 == ex.code:
+                if not (url in self.list):
+                    self.list.append(url)
+            else:
+                log(str(ex))
+            return
+
+        r = r.read().decode('utf-8')
         anchors = []
         for anchor in anchor_re.findall(r):
             a = anchor.replace(self.root, '')
@@ -86,20 +119,25 @@ class Crawler(Thread):
         count = len(self.list)
         self.file.write(url + '\n')
         if 0 == count % 50:
-            progress = round((datetime.now() - self.last).seconds/10) * '▯'
-            print('\t'.join([self.name, str(count), str(len(self.queue)), progress]))
+            now = datetime.now()
+            progress = round((now - self.last).seconds/10) * '▯'
+            self.last = now
+            progress = [self.name, str(count), str(len(self.queue)), progress]
+            if self.snooze_summary > 0:
+                progress.append(str(round(self.snooze_summary * 10)/10))
+                self.snooze_summary = 0
+            print('\t'.join(progress))
             self.save()
 
     @staticmethod
     def schedule(url):
-        crc = crc32(url.encode('ascii', 'ignore'))
+        crc = crc32(url.encode('ascii'))
         crawler = Crawler.threads[crc % len(Crawler.threads)]
         if not (url in crawler.list or url in crawler.queue):
             crawler.queue.appendleft(url)
 
     def save(self):
         self.save_list(self.queue_filename, self.queue)
-        self.last = datetime.now()
 
     @staticmethod
     def load_list(filename):
